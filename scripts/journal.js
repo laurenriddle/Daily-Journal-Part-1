83aeea0e06224fe7c26f9ebea7386932cff385b2
Lauren Riddle <lari7132@gmail.com>
2019-10-28 15:45:38 -0500

    formatted document:

--- scripts/journal.js
+++ scripts/journal.js
@@ -4,16 +4,16 @@
 */
 const journalEntry = [
     {
-    "Date of Entry": "10/10/2019",
-    "Concepts Covered": "JS Objects",
-    "Journal Entry": "Today, we learned about looping through JS objects using a 'for in' loop. When you loop through an object, it will output the key and the value.",
-    "Mood for the Day": "Happy"
-}, {
-    "Date of Entry": "10/10/2019",
-    "Concepts Covered": "JS Objects",
-    "Journal Entry": "Today, we learned about looping through JS objects using a 'for in' loop. When you loop through an object, it will output the key and the value.",
-    "Mood for the Day": "Happy"  
-}
+        "Date of Entry": "10/10/2019",
+        "Concepts Covered": "JS Objects",
+        "Journal Entry": "Today, we learned about looping through JS objects using a 'for in' loop. When you loop through an object, it will output the key and the value.",
+        "Mood for the Day": "Happy"
+    }, {
+        "Date of Entry": "10/10/2019",
+        "Concepts Covered": "JS Objects",
+        "Journal Entry": "Today, we learned about looping through JS objects using a 'for in' loop. When you loop through an object, it will output the key and the value.",
+        "Mood for the Day": "Happy"
+    }
 ]
 
 
